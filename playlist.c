7468bf83416c9860d413cb6de028ab725c2596ee
Ethan Lee <elee52@stuy.edu>
2025-01-13 14:19:42 -0500

    created a way to grab mp3 files by checking if the name ends in 3

--- playlist.c
+++ playlist.c
@@ -1,13 +1,32 @@
 #include "node.h"
 #include "library.h"
 #include <dirent.h>
+#include <stdio.h>
+#include <string.h>
 int createPlayList(){
     struct song_node ** library = init();
 //go through the mp3 files and add them to library
-int num = 0;
 //num needs to be the number of the mp3file; also when u add it will auto sort so you cant just keep track of it in one place
 //also that value int file does nothing as of right now, there is no purpose yet. 
-    add(library,"artist","sample",num);
+//DIRENT
+  DIR * d;
+  char* PATH = ".";
+  d = opendir( PATH );
+  struct dirent *entry;
+
+  //Can use as a while boolean
+  //since null is the end of the dirent list.
+  while(entry = readdir( d )){
+    char* name = entry->d_name;
+    if (name[strlen(name)-1] == '3' ){
+        printf("gotcha: ");
+        printf("filename: %s\n",name);
+    }
+    
+    //printf("--type: %d\n",entry->d_type);
+    //You can use the PATH + filename to get stat data!
+  }
+  closedir(d);
     print_library(library);
     return 0;
 }
