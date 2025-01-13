8db13470cf4efd4f771887487206cc49b951cbaa
Ethan Lee <elee52@stuy.edu>
2025-01-12 23:49:01 -0500

    test, need path

--- main.c
+++ main.c
@@ -12,5 +12,23 @@ int main(){
     // //fork and execvp
     char *filePath = NULL;
     char *const args[] = {"mpg123",(char *)filePath,NULL};
+
+    pid_t pid = fork();
+
+    if (pid == -1) {//fail
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        // Child process (executes mpg123)
+        if (execvp("mpg123", args) == -1) {
+            // If execvp fails
+            perror("execvp failed");
+            exit(EXIT_FAILURE);
+        }
+    } else {
+        printf("Playing the song...\n");
+        wait(NULL);
+        printf("Finished playing the song.\n");
+    }
   return 0;
 }
