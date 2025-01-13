4f994a1ac509dd9a4f1e3d5fba1e03166ae742e8
Ethan Lee <elee52@stuy.edu>
2025-01-12 23:26:08 -0500

    worked on single test

--- main.c
+++ main.c
@@ -4,11 +4,13 @@
 #include "node.h"
 #include "library.h"
 #include <ctype.h>
+#include  <unistd.h>
 
 int main(){
-    createPlayList();
-    printf("UI:");
-    //fork and execvp
-    
+    // createPlayList();
+    // printf("UI:");
+    // //fork and execvp
+    char *filePath = NULL;
+    char *const args[] = {"mpg123",(char *)filePath,NULL};
   return 0;
 }
